8079a8ef5ed1116ece589cd71f0001d6a41d3d6d
Annamaria Koshy <ak6429@nyu.edu>
2019-02-21 11:17:48 -0500

    Bus Movement & Basic Camera etc

---
+++ Assets/Scripts/TankControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControl : MonoBehaviour
+{
+    public Vector2 inputAxis;
+    public Rigidbody rb;
+
+    public float tankMax = 4f;
+    public float turnAdj;
+
+    public float forceMod;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputAxis.y = Input.GetAxis("Vertical");
+        inputAxis.x = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        if (rb.velocity.magnitude < tankMax)
+        {
+            rb.AddForce(transform.forward * inputAxis.y * forceMod, ForceMode.Impulse);
+        }
+
+        this.transform.Rotate(new Vector3(0, 1, 0), inputAxis.x * turnAdj);
+        //rb.AddTorque(0, inputAxis.x * turnAdj, 0, ForceMode.Impulse);
+
+    }
+}
