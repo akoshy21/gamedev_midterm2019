eb45ca33b1d0bfda1e892979471a7596852b4a79
Annamaria Koshy <ak6429@nyu.edu>
2019-02-26 12:06:39 -0500

    implemented damage system and movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float forceMod;
+
+    void Update()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * forceMod * Time.deltaTime, ForceMode.Force);
+
+        Debug.Log("MOVE");
+    }
+}
--- Assets/Scripts/VerticaCollisionScript.cs
+++ Assets/Scripts/VerticaCollisionScript.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class VerticaCollisionScript : MonoBehaviour
 {
     public float forceMod;
+    public Material red;
+    public Material orange;
+
+    public int dmg = 1;
+
+    private void Update()
+    {
+        CheckDmg();
+    }
 
     void OnCollisionEnter(Collision col)
     {
@@ -13,6 +22,26 @@ public class VerticaCollisionScript : MonoBehaviour
         {
             Debug.Log("COLLIDE");
             this.GetComponent<Rigidbody>().AddForce(transform.up * forceMod, ForceMode.Force);
+
+            if (dmg < 3)
+            {
+                dmg++;
+            }
+        }
+    }
+
+
+    // change this out from material to model
+    void CheckDmg()
+    {
+        if (dmg == 2)
+        {
+            Debug.Log("ORANGE");
+            GetComponent<MeshRenderer>().material = orange;
+        }
+        else if (dmg == 3)
+        {
+            GetComponent<MeshRenderer>().material = red;
         }
     }
 }
